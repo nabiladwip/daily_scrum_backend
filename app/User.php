dc1e04c6d6ab0de54e8691090bd8076c0f0782e3
nabiladepee <git>
2020-03-31 12:25:20 +0700

    second commit

--- app/User.php
+++ app/User.php
@@ -6,19 +6,23 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
+    protected $table = "users";
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname','lastname', 'email', 'password','password_verify',
     ];
 
+    
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -36,4 +40,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return[];
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('register', 'UserController@register');
+Route::post('login', 'UserController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::middleware(['jwt.verify'])->group(function(){
+    //user
+    Route::get('user', "UserController@getAll");
+	Route::delete('user/delete/{id}', "UserController@delete");
+	Route::post('user/ubah', "UserController@ubah");
+	Route::get('user/data', "UserController@index");
+
+
+    
+
+
 });
